6d7f999ec741d026f41ff6d604c17c610f765a07
Casey Fayard <fayardcasey@gmail.com>
2020-06-21 13:51:20 -0400

    factories

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1,
-  },
-  draw: function () {
-    console.log("draw");
-  },
-};
-
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function () {
+      console.log("draw");
+    },
+  };
+}
+const circle = createCircle(1);
 circle.draw();
